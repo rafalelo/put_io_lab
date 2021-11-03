6075c1437997792fcb330d857762f93319b9f9b5
Rafal <rafaubolek@gmail.com>
2021-11-03 12:30:49 +0100

    change 2

--- 04-git/Program.java
+++ 04-git/Program.java
@@ -3,7 +3,7 @@ public class Main {
 
   public static void main(String[] args) { //comment
     Main myObj1 = new Main();  // Object 1
-    Main myObj2 = new Main();  // Object 2
+    Main myObj3 = new Main();  // Object 2
     System.out.println(myObj1.x);
     System.out.println(myObj2.x);
   }
