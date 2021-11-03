0d47ca59bc9a2d70270674d4a57822917d32249e
Rafal <rafaubolek@gmail.com>
2021-11-03 12:30:30 +0100

    change 1

--- 04-git/Program.java
+++ 04-git/Program.java
@@ -1,7 +1,7 @@
 public class Main {
   int x = 5;
 
-  public static void main(String[] args) {
+  public static void main(String[] args) { //comment
     Main myObj1 = new Main();  // Object 1
     Main myObj2 = new Main();  // Object 2
     System.out.println(myObj1.x);
